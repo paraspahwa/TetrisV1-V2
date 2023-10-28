f4cfe8fc50a598ddf36788e072a29805ada60d07
Paras Pahwa <134779342+paraspahwa@users.noreply.github.com>
2023-10-28 09:11:57 +0530

    Update backend.tf

--- Jenkins-Terrform/backend.tf
+++ Jenkins-Terrform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "" # Replace with your actual S3 bucket name
+    bucket = "tetris-file-info" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
